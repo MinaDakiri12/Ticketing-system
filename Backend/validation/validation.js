ce21315554fa1c2b9c6e0b84c6c43ed6ddaa8243
MinaDakiri <minadakiri7@gmail.com>
2021-05-27 12:21:12 +0100

    Create validation.js

---
+++ Backend/validation/validation.js
@@ -0,0 +1,44 @@
+const Joi = require('joi');
+
+exports.registerValidation = data=>{
+
+    const schema = Joi.object({
+    full_name: Joi.string().min(3).max(100).trim().required(),
+    email: Joi.string().email().required(),
+    type: Joi.string(),
+    password: Joi.string().min(6).required(),
+    department: Joi.string()
+})
+    return schema.validate(data)
+}
+
+exports.loginValidation = data=>{
+
+    const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+})
+    return schema.validate(data)
+}
+
+exports.ticketValidation = data=>{
+
+    const schema = Joi.object({
+    titre: Joi.string().min(6).max(100).required(),
+    type: Joi.string().min(4).max(100).required(),
+    emergency: Joi.string(),
+    description: Joi.string().min(10).max(1024).required(),
+    state: Joi.string(),
+    
+})
+    return schema.validate(data)
+}
+
+exports.departmentValidation = data=>{
+
+    const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    
+})
+    return schema.validate(data)
+}
