29fd31c142e9d9bd85d735a8732a49d1601a0630
MinaDakiri <minadakiri7@gmail.com>
2021-05-27 12:20:51 +0100

    Create Department.js

---
+++ Backend/models/Department.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const departmentSchema = Schema({
+    name: {type: String, min:3, max: 100, require: true},
+})
+
+module.exports = model('Department', departmentSchema)
