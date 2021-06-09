4d180ac78e7b080ee1213b6c6ca2a18c6590c962
MinaDakiri <minadakiri7@gmail.com>
2021-06-09 12:18:44 +0100

    Create NavEmp.jsx

---
+++ front-end/src/component/Employer/NavEmp.jsx
@@ -0,0 +1,42 @@
+import React, {Fragment} from 'react'
+import { Link, withRouter} from 'react-router-dom'
+
+
+
+const NavEmp = (props) =>{
+
+    return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ">
+              <Fragment>  
+                <li className="nav-item active">
+                  <Link  className="nav-link" to="/create-tickets">Create Tickets <span className="sr-only">(current)</span></Link >
+                </li>
+              
+              </Fragment>
+              </ul>
+              <ul className="navbar-nav ml-auto">
+              <Fragment>  
+                <li className="nav-item active ml-auto">
+                  <Link  className="nav-link" to="/logout"> <i className="fas fa-sign-out-alt mr-1"></i>Logout<span className="sr-only">(current)</span></Link >
+                </li>
+              
+              </Fragment>
+              
+
+            
+            </ul>
+           
+            
+          </div>
+      </nav>      
+     </div>
+    )
+}
+
+export default withRouter(NavEmp)
