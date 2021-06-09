5eeb9a9448260b37cbcfdb36087ce62b3cfe680f
MinaDakiri <minadakiri7@gmail.com>
2021-06-09 14:28:44 +0100

    Update route.js

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {logout, signin, signup} = require('../controllers/authController');
 const {verificationToken} = require('../middlewares/Auth')
-const {addDepartment, assignTicket, getAssignedTicket, resolvedTicket, cancelTicket, getDepartment, getTechnician, addTicket, getTicketById, getTicket, getEmployedTicket,deleteTicket,refuseTicket,acceptTicket} = require('../controllers/employerController')
+const {addDepartment, assignTicket, getAssignedTicket, resolvedTicket, cancelTicket, getDepartment, getTechnician, addTicket, getTicketById, getTicket, getEmployedTicket,deleteTicket,refuseTicket,acceptTicket,getAllUser} = require('../controllers/employerController')
 
 router.post('/login', signin);
 router.post('/logout', logout);
@@ -26,6 +26,7 @@ router.post('/addDepartment', verificationToken('admin'), addDepartment);
 router.post('/assign/:id', verificationToken('admin'), assignTicket);
 router.get('/department', verificationToken('admin'), getDepartment);
 router.get('/technician', verificationToken('admin'), getTechnician);
+router.get('/users', verificationToken('admin'), getAllUser);
 router.get('/ticket', verificationToken('admin'), getTicket);
 router.get('/getTicket/:id', verificationToken('admin'), getTicketById);
 router.get('/refuse/:id',  verificationToken('admin'),refuseTicket)
