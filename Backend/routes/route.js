775d9bccb60eacd0fb7cbe2a73d118bc88a773f4
MinaDakiri <minadakiri7@gmail.com>
2021-06-09 12:14:13 +0100

    Update route.js

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -2,21 +2,35 @@ const express = require('express');
 const router = express.Router();
 const {logout, signin, signup} = require('../controllers/authController');
 const {verificationToken} = require('../middlewares/Auth')
-const {addDepartment, assignTicket, getAssignedTicket, resolvedTicket, cancelTicket, getDepartment, getTechnician, addTicket, getTicketById, getTicket} = require('../controllers/employerController')
+const {addDepartment, assignTicket, getAssignedTicket, resolvedTicket, cancelTicket, getDepartment, getTechnician, addTicket, getTicketById, getTicket, getEmployedTicket,deleteTicket,refuseTicket,acceptTicket} = require('../controllers/employerController')
 
 router.post('/login', signin);
-router.post('/signup',verificationToken('admin'), signup);
 router.post('/logout', logout);
-router.post('/addDepartment', verificationToken('admin'), addDepartment);
-router.post('/assign', verificationToken('admin'), assignTicket);
-router.post('/addTicket', verificationToken('employer'), addTicket);
+
+//Employer
+router.post('/addTicket',verificationToken('employer'), addTicket);
+router.get('/empTicket', verificationToken('employer'), getEmployedTicket);
+router.delete('/deleteTicket/:id',verificationToken('employer'),deleteTicket)
+
+//Technician
 router.post('/cancelTicket/:id', verificationToken('technician'), cancelTicket);
 router.post('/resolvedTicket/:id', verificationToken('technician'), resolvedTicket);
 router.get('/technicianTicket', verificationToken('technician'), getAssignedTicket);
+
+
+
+//admin 
+router.post('/signup',verificationToken('admin'), signup);
+router.post('/addDepartment', verificationToken('admin'), addDepartment);
+router.post('/addDepartment', verificationToken('admin'), addDepartment);
+router.post('/assign/:id', verificationToken('admin'), assignTicket);
 router.get('/department', verificationToken('admin'), getDepartment);
 router.get('/technician', verificationToken('admin'), getTechnician);
 router.get('/ticket', verificationToken('admin'), getTicket);
-router.get('/ticket/:id', verificationToken('admin'), getTicketById);
+router.get('/getTicket/:id', verificationToken('admin'), getTicketById);
+router.get('/refuse/:id',  verificationToken('admin'),refuseTicket)
+router.get('/accept', verificationToken('admin'), acceptTicket)
+
 
 
 module.exports = router;
