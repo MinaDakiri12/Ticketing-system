e05e61f5fcb7b01bad078f28dd621261944b994a
MinaDakiri <minadakiri7@gmail.com>
2021-06-09 12:18:58 +0100

    Create Home.jsx

---
+++ front-end/src/core/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from './Layout'
+import Menu from './Menu'
+
+
+const Home = () =>{
+    return (
+      <>
+        <Menu/>
+      
+         
+          <div>
+            <Layout title="Ticketing System "></Layout>
+          </div>
+        
+      </>
+    )
+}
+
+export default Home
