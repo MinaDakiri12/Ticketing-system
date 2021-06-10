c52e49f33f339b85d86b6d02e11d97ea4c77ceb3
MinaDakiri <minadakiri7@gmail.com>
2021-06-10 15:10:17 +0100

    Create UserContextApi.js

---
+++ front-end/src/component/UserContextApi.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+// create context
+export const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+
+
+  const [auth, setAuth] = useState({ isAuth: false, type: "" });
+
+  return (
+    <UserContext.Provider value={{ auth,setAuth}}>
+      {children}
+    </UserContext.Provider>
+  );
+};
