d8f4a854d0e657bb66757f29b8af42086cc4aeb6
MinaDakiri <minadakiri7@gmail.com>
2021-06-11 13:33:06 +0100

    Update Department.js

--- Backend/models/Department.js
+++ Backend/models/Department.js
@@ -3,6 +3,7 @@ const {Schema, model} = mongoose;
 
 const departmentSchema = Schema({
     name: {type: String, min:3, max: 100, require: true},
+    responsible: {type: String, min:3, max: 100, require: true}
 })
 
 module.exports = model('Department', departmentSchema)
